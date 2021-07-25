7c38891c6d74e0f3ad6de7a6732616bc74915e4c
Aarya <49730497+aaryalamkhade@users.noreply.github.com>
2021-07-25 22:06:56 +0530

    Add files via upload

---
+++ src/com.IIHT_Project.model/students.java
@@ -0,0 +1,48 @@
+package com.IIHT_Project.model;
+
+public class students 
+{
+	private String first_Name;
+    private String last_Name;
+    private String username;
+    private String password;
+    private String address;
+    private String contact;
+	public String getFirst_Name() {
+		return first_Name;
+	}
+	public void setFirst_Name(String first_Name) {
+		this.first_Name = first_Name;
+	}
+	public String getLast_Name() {
+		return last_Name;
+	}
+	public void setLast_Name(String last_Name) {
+		this.last_Name = last_Name;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getContact() {
+		return contact;
+	}
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+    
+}
